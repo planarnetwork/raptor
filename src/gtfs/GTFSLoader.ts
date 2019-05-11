9350e46840f450b72b1099c7f4cc40633801f101
Linus Norton <linusnorton@gmail.com>
2019-05-11 21:03:27 +0100

    Fixed a bug where the arrival time was always set to the departure time, minor tidy up, minor micro optimizations

--- src/gtfs/GTFSLoader.ts
+++ src/gtfs/GTFSLoader.ts
@@ -24,7 +24,7 @@ export function loadGTFS(stream: Readable): Promise<GTFSData> {
       const t = {
         origin: row.from_stop_id,
         destination: row.to_stop_id,
-        duration: parseInt(row.duration, 10),
+        duration: +row.duration,
         startTime: timeParser.getTime(row.start_time),
         endTime: timeParser.getTime(row.end_time)
       };
@@ -34,8 +34,8 @@ export function loadGTFS(stream: Readable): Promise<GTFSData> {
     calendar: row => {
       calendars[row.service_id] = {
         serviceId: row.service_id,
-        startDate: parseInt(row.start_date, 10),
-        endDate: parseInt(row.end_date, 10),
+        startDate: +row.start_date,
+        endDate: +row.end_date,
         days: {
           0: row.sunday === "1",
           1: row.monday === "1",
@@ -61,23 +61,22 @@ export function loadGTFS(stream: Readable): Promise<GTFSData> {
       const stopTime = {
         stop: row.stop_id,
         departureTime: timeParser.getTime(row.departure_time),
-        arrivalTime: timeParser.getTime(row.departure_time),
+        arrivalTime: timeParser.getTime(row.arrival_time),
         pickUp: row.pickup_type === "0",
         dropOff: row.drop_off_type === "0"
       };
 
-      stopTimes[row.trip_id] = stopTimes[row.trip_id] || [];
-      stopTimes[row.trip_id].push(stopTime);
+      pushNested(stopTime, stopTimes, row.trip_id);
     },
     transfer: row => {
       if (row.from_stop_id === row.to_stop_id) {
-        interchange[row.from_stop_id] = parseInt(row.min_transfer_time, 10);
+        interchange[row.from_stop_id] = +row.min_transfer_time;
       }
       else {
         const t = {
           origin: row.from_stop_id,
           destination: row.to_stop_id,
-          duration: parseInt(row.min_transfer_time, 10),
+          duration: +row.min_transfer_time,
           startTime: 0,
           endTime: Number.MAX_SAFE_INTEGER
         };
@@ -91,15 +90,13 @@ export function loadGTFS(stream: Readable): Promise<GTFSData> {
         code: row.stop_code,
         name: row.stop_name,
         description: row.stop_desc,
-        latitude: Number(row.stop_lat),
-        longitude: Number(row.stop_lon),
+        latitude: +row.stop_lat,
+        longitude: +row.stop_lon,
         timezone: row.zone_id
       };
 
       setNested(stop, stops, row.stop_id);
-    },
-    route: () => {},
-    agency: () => {},
+    }
   };
 
   return new Promise(resolve => {
